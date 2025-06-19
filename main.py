53a733402de31e1af8832dc6edf5826d71fc37ab
Joshua Wentzell <wentzelljoshua047@gmail.com>
2025-06-18 22:38:43 -0400

    Add system prompt

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from google.genai import types
 from dotenv import load_dotenv
 from google import genai
 verbose = False
+system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -18,9 +19,10 @@ else:
     sys.exit(1)
 messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)]),]
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt))
 print(response.text)
 if verbose:
     print(f"User prompt: {prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if response.usage_metadata:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
